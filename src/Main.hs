6ec3e0894186049c41f9291d59f44f912d8dcd77
松原信忠 <t12307043@gunma-u.ac.jp>
2017-09-09 10:59:40 +0900

    Add sample servant api server
    ref: http://qiita.com/lotz/items/883b41fa79f060e59efa

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,10 @@
 module Main where
 
+import           API                      (api, server)
+import qualified Network.Wai.Handler.Warp as Warp
+import           Servant.Server           (serve)
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Listening on port 8080"
+  Warp.run 8080 $ serve api server
